5ea5c7f484664f8ace4d1876cb40ea6e4f7fe44c
GodUser1005 <jijone1005@naver.com>
2023-07-19 23:26:19 +0900

    초기화

--- src/Customer.java
+++ src/Customer.java
@@ -1,5 +1,40 @@
-public abstract class Customer {
+public abstract class Customer implements PaymentWithBonus,PaymentWithDiscount{
+
+    // State
     private String name;
     private Grade grade;
-    private howToDiscount discountWay;
+    private DiscountWay discountWay;
+
+    // Creation
+    Customer(String name, Grade grade,DiscountWay discountWay){
+        this.name = name;
+        this.grade = grade;
+        this.discountWay = discountWay;
+    }
+    // Operation
+    public abstract void printUserInfo();
+
+
+
+    // getters, setters
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setGrade(Grade grade) {
+        this.grade = grade;
+    }
+    public Grade getGrade() {
+        return grade;
+    }
+
+    public void setDiscountWay(DiscountWay discountWay) {
+        this.discountWay = discountWay;
+    }
+    public DiscountWay getDiscountWay() {
+        return discountWay;
+    }
 }
